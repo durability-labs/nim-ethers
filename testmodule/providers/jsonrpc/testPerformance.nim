b4b0604ff0a900ecf70fd639e768f64e2093b423
Mark Spanbroek <mark@spanbroek.net>
2026-02-04 10:18:29 +0100

    chore: fix typo

--- testmodule/providers/jsonrpc/testPerformance.nim
+++ testmodule/providers/jsonrpc/testPerformance.nim
@@ -4,7 +4,7 @@ import ethers
 
 suite "JSON-RPC performance":
 
-  test "can handle 50.000 concurrent connections":
+  test "can handle 50 000 concurrent connections":
     let provider = await JsonRpcProvider.connect("http://localhost:8545")
     let futures = newSeqWith(50_000, provider.getChainId())
     await allFutures(futures)
