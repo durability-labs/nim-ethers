18e3be0399028c0c445f39aeadc62dd13a5db116
markspanbroek <mark@spanbroek.net>
2026-02-10 15:31:34 +0000

    fix: enable HTTP 1.1 pipelining (#6)
    * fix: enable HTTP 1.1 pipelining
    
    reason: avoids connection timeouts with
    concurrent rpc connections
    
    * feat: configurable HTTP pipelining and HTTP concurrency limit

--- testmodule/providers/jsonrpc/testPerformance.nim
+++ testmodule/providers/jsonrpc/testPerformance.nim
@@ -4,9 +4,10 @@ import ethers
 
 suite "JSON-RPC performance":
 
-  test "can handle 50 000 concurrent connections":
-    let provider = await JsonRpcProvider.connect("http://localhost:8545")
-    let futures = newSeqWith(50_000, provider.getChainId())
+  test "can handle 50 000 simultaneous connections by limiting concurrency":
+    let options = JsonRpcOptions(httpConcurrencyLimit: some 100)
+    let provider = await JsonRpcProvider.connect("http://localhost:8545", options)
+    let futures = newSeqWith(50_000, provider.getBlockNumber())
     await allFutures(futures)
     check allIt(futures, it.completed)
     await provider.close()
