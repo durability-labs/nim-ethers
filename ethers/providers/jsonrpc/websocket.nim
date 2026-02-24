b25a170f4efa82d3dc09f96eeeff936d134b61c5
Mark Spanbroek <mark@spanbroek.net>
2026-02-24 15:19:30 +0100

    Revert "chore: use new json_rpc router mechanism"
    This reverts commit 38dc75b0155f35fca6be1a1a4dcefd71471f0157.

--- ethers/providers/jsonrpc/websocket.nim
+++ ethers/providers/jsonrpc/websocket.nim
@@ -1,18 +1,34 @@
+import std/json
 import pkg/json_rpc/rpcclient
-import pkg/json_rpc/router
 import ../../basics
 import ../../subscriptions
 import ./rpccalls
 import ./errors
 
-proc rpcRouter*(subscriptions: Subscriptions): ref RpcRouter =
-  let router = RpcRouter.new()
-  router[].rpc("eth_subscription") do() -> void:
-    subscriptions.update()
-  router
-
-proc subscribeBlockNotifications*(
+proc useWebsocketUpdates*(
+  subscriptions: Subscriptions,
   websocket: RpcWebSocketClient
 ) {.async:(raises:[JsonRpcProviderError, CancelledError]).} =
+  var rpcSubscriptionId: JsonNode
+
+  proc processMessage(client: RpcClient, message: string): Result[bool, string] =
+    without message =? parseJson(message).catch:
+      return ok true
+    without rpcMethod =? message{"method"}:
+      return ok true
+    if rpcMethod.getStr() != "eth_subscription":
+      return ok true
+    without rpcParameter =? message{"params"}{"subscription"}:
+      return ok true
+    if rpcParameter != rpcSubscriptionId:
+      return ok true
+
+    subscriptions.update()
+
+    ok false # do not process further using json-rpc default handler
+
+  assert websocket.onProcessMessage.isNil
+  websocket.onProcessMessage = processMessage
+
   convertError:
-    discard await websocket.eth_subscribe("newHeads")
+    rpcSubscriptionId = await websocket.eth_subscribe("newHeads")
